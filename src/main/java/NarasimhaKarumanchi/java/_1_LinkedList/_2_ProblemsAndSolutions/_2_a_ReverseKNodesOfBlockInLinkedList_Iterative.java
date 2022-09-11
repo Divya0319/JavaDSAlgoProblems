4ec57ec7cd1a2d91e9cff96d0d47443f11326712
Divya Gupta <divygupta0319@gmail.com>
2022-09-11 17:52:32 +0530

    Included problem of reversing k blocks of nodes in linked list

---
+++ src/main/java/NarasimhaKarumanchi/java/_1_LinkedList/_2_ProblemsAndSolutions/_2_a_ReverseKNodesOfBlockInLinkedList_Iterative.java
@@ -0,0 +1,70 @@
+package main.java.NarasimhaKarumanchi.java._1_LinkedList._2_ProblemsAndSolutions;
+
+import main.java.NarasimhaKarumanchi.java._1_LinkedList.LinkedList;
+import main.java.NarasimhaKarumanchi.java._1_LinkedList.ListNode;
+
+public class _2_a_ReverseKNodesOfBlockInLinkedList_Iterative<T> {
+
+	public ListNode<T> reverseKNodes(ListNode<T> head, int k) {
+		
+		if(head == null || k == 1) return head;
+				
+		ListNode<T> dummy = new ListNode<>((T)new Integer(0));
+		dummy.setNext(head);
+		
+		ListNode<T> curr = dummy, nex = dummy, pre = dummy;
+		int count = 0;
+		
+		while(curr.getNext() != null) {
+			curr = curr.getNext();
+			count++;
+		}
+		
+		while(count >= k) {
+			curr = pre.getNext();
+			nex = curr.getNext();
+			
+			for(int i = 1; i < k; i++) {
+				curr.setNext(nex.getNext());
+				nex.setNext(pre.getNext());
+				pre.setNext(nex);
+				nex = curr.getNext();
+			}
+			
+			pre = curr;
+			count -= k;
+		}
+		
+
+		return dummy.getNext();
+	}
+
+	public static void main(String[] args) {
+		_2_a_ReverseKNodesOfBlockInLinkedList_Iterative<Integer> mainClass = new _2_a_ReverseKNodesOfBlockInLinkedList_Iterative<>();
+		LinkedList<Integer> ll = new LinkedList<>();
+		ll.insertAtBegin(1);
+		System.out.println(ll.toString());
+		ll.insertAtEnd(2);
+		System.out.println(ll.toString());
+		ll.insertAtEnd(3);
+		System.out.println(ll.toString());
+		ll.insertAtEnd(4);
+		System.out.println(ll.toString());
+		ll.insertAtEnd(5);
+		System.out.println(ll.toString());
+		ll.insertAtEnd(6);
+		System.out.println(ll.toString());
+		ll.insertAtEnd(7);
+		System.out.println(ll.toString());
+		ll.insertAtEnd(8);
+		System.out.println(ll.toString());
+		ll.insertAtEnd(9);
+		System.out.println(ll.toString());
+		 
+		int k = 4;
+		ListNode<Integer> exchangedList = mainClass.reverseKNodes(ll.getHead(), k);
+		System.out.println("Linked list after reversing " + k + "blocks of nodes: " + ll.toString(exchangedList));
+
+	}
+
+}
