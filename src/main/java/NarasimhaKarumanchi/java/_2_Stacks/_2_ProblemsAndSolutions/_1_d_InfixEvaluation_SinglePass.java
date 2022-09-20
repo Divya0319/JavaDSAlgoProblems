05c3b2fa926bc51c1d8b3c94723f961853409273
Divya Gupta <divygupta0319@gmail.com>
2022-09-20 17:27:43 +0530

    Added infix evaluation in single pass using stack problem

--- src/main/java/NarasimhaKarumanchi/java/_2_Stacks/_2_ProblemsAndSolutions/_1_d_InfixEvaluation_SinglePass.java
+++ src/main/java/NarasimhaKarumanchi/java/_2_Stacks/_2_ProblemsAndSolutions/_1_d_InfixEvaluation_SinglePass.java
@@ -4,32 +4,116 @@ import main.java.NarasimhaKarumanchi.java._2_Stacks.LinkedStack;
 
 public class _1_d_InfixEvaluation_SinglePass {
 	
-	public int evaluatePostFix(String s) {
-		LinkedStack<Integer> stk = new LinkedStack<>();
-		for(int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
+	public int infixEvaluationIn1Pass(String expr) {
+		
+		// Created two stacks
+		// one for storing operands, other for storing operands
+		LinkedStack<Integer> opnds = new LinkedStack<>();
+		LinkedStack<Character> optors = new LinkedStack<>();
+		for(int i = 0; i < expr.length(); i++) {
+			char ch = expr.charAt(i);
 			
-			if(Character.isDigit(c)) {
-				stk.push(c- '0');
-			} else {
-				int val1 = stk.pop();
-				int val2 = stk.pop();
-				switch(c) {
-				case '-' : stk.push(val2-val1);
-				break;
-				case '+' : stk.push(val2+val1);
-				break;
-				case '*' : stk.push(val2*val1);
-				break;
-				case '/' : stk.push(val2/val1);
-				break;
-				case '^' : stk.push(pow(val2, val1));
-				break;
+			// if i/p character is '(',
+			// push into operator stack
+			if(ch == '(') {
+				optors.push(ch);
+				
+			// but if i/p character is digit
+			// push into operand stack, after converting to number
+			} else if(Character.isDigit(ch)) {
+				opnds.push(ch - '0');
+				
+			// when i/p character is ')',
+			// until on top of stack '(' is seen,
+			// pop two operands from operand stack, v1 & v2
+			// and one operator from operator stack
+			// perform operation as v2 op v1,
+			// and put result into operand stack
+			} else if(ch == ')') {
+				while(optors.peek() == '(') {
+					char optor = optors.pop();
+					int v2 = opnds.pop();
+					int v1 = opnds.pop();
+					
+					opnds.push(operation(v1, v2, optor));
 				}
+				
+				// pop '(' from operator stack as well
+				optors.pop();
+				
+			// if i/p character is any of +, -, *, / or ^
+			} else if(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^') {
+				
+				// ch is wanting higher priority operator to solve first
+				while(optors.size() > 0 && optors.peek() != '(' && Prec(ch) <= Prec(optors.peek())) {
+					char optor = optors.pop();
+					int v2 = opnds.pop();
+					int v1 = opnds.pop();
+					
+					opnds.push(operation(v1, v2, optor));
+				}
+				
+				// ch is pushing itself now
+				
+				optors.push(ch);
 			}
 		}
 		
-		return stk.pop();
+		
+		// if any operators left in stack
+		while(optors.size() != 0) {
+			
+			// pop operands and operators
+			char optor = optors.pop();
+			int v2 = opnds.pop();
+			int v1 = opnds.pop();
+			
+			
+			// perform operation and push into operand stack
+			opnds.push(operation(v1, v2, optor));
+		}
+		
+		
+		// after operator stack is empty,
+		// return the result 
+		return opnds.peek();
+			
+	}
+	
+	/*	Precedence order
+	 *    '^'   >   '/'  >  '*'  >  '+/-'
+	 */
+	public int Prec(char c) {
+		switch(c) 
+		{
+		case '+':
+		case '-':
+			return 1;
+		case '*':
+		case '/':	
+			return 2;
+		case '^':
+			return 3;
+		default:
+			return -1;
+		}
+	}
+	
+	public int operation(int val1, int val2, char op) {
+		switch(op) {
+		case '-' : return val1-val2;
+
+		case '+' : return val1+val2;
+
+		case '*' : return val1*val2;
+
+		case '/' : return val1/val2;
+
+		case '^' : return pow(val1, val2);
+
+		default:
+			return -1;
+		}
 	}
 	
 	public int pow(int val1, int val2) {
@@ -45,9 +129,9 @@ public class _1_d_InfixEvaluation_SinglePass {
 		
 		_1_d_InfixEvaluation_SinglePass mainClass = new _1_d_InfixEvaluation_SinglePass();
 		
-		String exp = "252^+9-";
-		System.out.println("Given postfix expression: " + exp);
-        System.out.println("Result after evaluation: " + mainClass.evaluatePostFix(exp));
+		String exp = "2^3*5";
+		System.out.println("Given infix expression: " + exp);
+        System.out.println("Result after evaluation: " + mainClass.infixEvaluationIn1Pass(exp));
 
 	}
 
