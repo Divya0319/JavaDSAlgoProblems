31b7ab77c5cd02bb52656e57d28e30df30b97a8a
Divya Gupta <divygupta0319@gmail.com>
2022-11-15 18:04:08 +0530

    Added simple approach and explanation for merging 2 sorted linked lists

--- src/main/java/NarasimhaKarumanchi/java/Merge2SortedLinkedLists_UsingSimpleUnderstandableApproach.java
+++ src/main/java/NarasimhaKarumanchi/java/Merge2SortedLinkedLists_UsingSimpleUnderstandableApproach.java
@@ -14,21 +14,43 @@ public class Merge2SortedLinkedLists_UsingSimpleUnderstandableApproach {
 		// dummy node is used here to avoid handling null pointer exceptions
 		// while one of the list iterator reaches null
 		ListNode<Integer> dummy = new ListNode<>(-1);
+		// initially, prev will point to dummy
 		ListNode<Integer> prev = dummy;
+		
+		// For keeping original heads of list intact,
+		// we use iterators c2 and c2 for the lists
 		ListNode<Integer> c1 = l1, c2 = l2;
+		
+		// until we reach end of any 1 list, we continue
 		while(c1 != null && c2 != null) {
+			
+			// when 1st list node data less than 2nd list node data
 			if(c1.getData() <= c2.getData()) {
+				
+				// point next of prev to c1
 				prev.setNext(c1);
+				
+				// advance c1
 				c1 = c1.getNext();
 			} else {
+				
+				// else, point next of prev to c1
 				prev.setNext(c2);
+				
+				// advance c2
 				c2 = c2.getNext();
 			}
+			
+			// and every time, advance prev also
 			prev = prev.getNext();
 		}
 		
+		// whenever any of list pointer reaches null
+		// we can simply attach remaining nodes of other list
+		// to end of our resulting merged list
 		prev.setNext((c1 != null) ? c1 : c2);
 		
+		// head of merged list will always be next of dummy
 		return dummy.getNext();
 						
 	}
