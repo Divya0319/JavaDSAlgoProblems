f189036082a7d59c87d3357b1db8d4a52f53c836
Divya Gupta <divygupta0319@gmail.com>
2023-02-11 14:20:26 +0530

    Added three approaches with comment explanations, for left rotating an array by D places

--- src/main/java/NarasimhaKarumanchi/java/_0_Arrays/Problems_And_Solutions/_1_i_LeftRotateArrayByOne.java
+++ src/main/java/NarasimhaKarumanchi/java/_0_Arrays/Problems_And_Solutions/_1_i_LeftRotateArrayByOne.java
@@ -2,12 +2,20 @@ package main.java.NarasimhaKarumanchi.java._0_Arrays.Problems_And_Solutions;
 
 import java.util.Arrays;
 
+/**
+ * 
+ * Left rotation means counter clockwise rotation, means first element moves to last, and every other element 
+ * moves one position backwards
+ *
+ */
 public class _1_i_LeftRotateArrayByOne {
 
 	/**
 	 * First we store 1st element of array to temp variable
 	 * Then, we copy arr[i] to arr[i-1], from 2nd element to last element
 	 * In the end, we copy temp variable value to last element, arr[n-1] = temp
+	 * 
+	 * T.C. is clearly theta(n), and S.C. is theta(1), as only few variables used
 	 *
 	 */
 	static void leftRotateByOne(int[] arr) {
---
+++ src/main/java/NarasimhaKarumanchi/java/_0_Arrays/Problems_And_Solutions/_1_j_LeftRotateArrayByDPlaces_theta_NXD_Time.java
@@ -0,0 +1,37 @@
+package main.java.NarasimhaKarumanchi.java._0_Arrays.Problems_And_Solutions;
+
+import java.util.Arrays;
+
+public class _1_j_LeftRotateArrayByDPlaces_theta_NXD_Time {
+
+	/**
+	 * Calling left rotate by one, d times, will effectively left rotate by d times
+	 *
+	 */
+	static void leftRotateByDPlaces(int[] arr, int d) {
+
+		for(int i = 0; i < d; i++) {
+			leftRotateByOne(arr);
+		}
+		
+	}
+	
+	static void leftRotateByOne(int[] arr) {
+		int n = arr.length;
+		int temp = arr[0];
+		for(int i = 1; i < n; i++) {
+			arr[i-1] = arr[i];
+		}
+		
+		arr[n-1] = temp;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		int d = 3;
+		leftRotateByDPlaces(arr, d);
+		System.out.print("Left rotated Array by " + d + " places: " + Arrays.toString(arr));
+		
+	}
+
+}
