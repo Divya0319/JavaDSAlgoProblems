c47e36b38d68a4f6aa6b1fe640c0b120aeeb4a8b
Divya Gupta <divygupta0319@gmail.com>
2023-02-24 01:58:08 +0530

    Added method to check a string is palindrome or not using recursion

---
+++ src/main/java/NarasimhaKarumanchi/java/_0_Arrays/Problems_And_Solutions/_1_q_PalindromeCheckUsingRecursion.java
@@ -0,0 +1,31 @@
+package main.java.NarasimhaKarumanchi.java._0_Arrays.Problems_And_Solutions;
+
+
+public class _1_q_PalindromeCheckUsingRecursion {
+
+	/*
+	 * Takes O(n) worst case time(T(n) = T(n-2) + O(n)), 
+	 * and O(n) auxiliary space for recursion stack(takes n/2 space actually)
+	 */
+	static boolean isPalindrome(String s, int start, int end) {
+		
+		if(start >= end) // handling both cases here, start=end happens when string becomes of length 0 for even length,
+						// and start>end happened when string becomes of length 1 for odd length
+		{ 
+			return true; 
+		}
+		/*
+		 * Checking first start and end characters are same, if they are, then only we make call for 
+		 * check of shrunk string
+		 */
+		return (s.charAt(start) == s.charAt(end) && isPalindrome(s, start+1, end-1));
+		
+	}
+
+	public static void main(String[] args) {
+		String inp = "jabbaj";
+		System.out.println("Is " + inp + " palindrome? " + isPalindrome(inp, 0, inp.length() - 1));
+
+	}
+
+}
