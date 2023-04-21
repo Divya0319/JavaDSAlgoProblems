ab9beb50ed0ecdc1171a386a2a0b7e525a13e2a5
Divya Gupta <divygupta0319@gmail.com>
2023-04-21 11:19:45 +0530

    Added iterative method to find sum of digits of a number

--- src/main/java/NarasimhaKarumanchi/java/_0_Arrays/Problems_And_Solutions/_1_r_SumOfDigitsUsingRecursion.java
+++ src/main/java/NarasimhaKarumanchi/java/_0_Arrays/Problems_And_Solutions/_1_r_SumOfDigitsUsingRecursion.java
@@ -4,8 +4,9 @@ package main.java.NarasimhaKarumanchi.java._0_Arrays.Problems_And_Solutions;
 public class _1_r_SumOfDigitsUsingRecursion {
 
 	/*
-	 * Takes O(n) worst case time(T(n) = T(n-2) + O(n)), 
-	 * and O(n) auxiliary space for recursion stack(takes n/2 space actually)
+	 * If d is number of digits in n,
+	 * Takes O(d) worst case time, 
+	 * and O(d) auxiliary space for recursion stack(takes d+1 space actually)
 	 */
 	static int sumOfDigits(int n) {
 		
@@ -16,7 +17,7 @@ public class _1_r_SumOfDigitsUsingRecursion {
 //		}
 		
 		if(n <= 9) // handling case here, when n becomes single digit after reduction,
-				  // then we simply return n.
+				   // then we simply return n.
 		{ 
 			return n; 
 		}
