b7aecdc7071b5d2a803f5d94772b6456e85043e1
Divya Gupta <divygupta0319@gmail.com>
2023-05-06 18:00:20 +0530

    Added skeleton code for Chaining implementation in Hashing

---
+++ src/main/java/NarasimhaKarumanchi/java/hashing/_1_MyHash_Using_Chaining.java
@@ -0,0 +1,32 @@
+package main.java.NarasimhaKarumanchi.java.hashing;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class _1_MyHash_Using_Chaining {
+	
+	int BUCKET;
+	ArrayList<LinkedList<Integer>> table;
+	
+	public _1_MyHash_Using_Chaining(int b) {
+		BUCKET = b;
+		table = new ArrayList<>();
+		
+		for(int i = 0; i < b; i++) {
+			table.add(new LinkedList<Integer>());
+		}
+	}
+	
+	void insert(int key) {
+		
+	}
+	
+	boolean search(int key) {
+		return false;
+	}
+	
+	void remove(int key) {
+		
+	}
+
+}
