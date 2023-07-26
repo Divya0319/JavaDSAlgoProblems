3d10ea905baaf90014aa078ef1ddcfedd00dc3df
Divya Gupta <divygupta0319@gmail.com>
2023-07-26 17:20:42 +0530

    Practiced the problem is stack pairwise consecutive

--- src/main/java/NarasimhaKarumanchi/java/_2_Stacks/_1_a_TestClass.java
+++ src/main/java/NarasimhaKarumanchi/java/_2_Stacks/_1_a_TestClass.java
@@ -5,112 +5,50 @@ import java.util.EmptyStackException;
 
 public class _1_a_TestClass<T>  {
 	
-	int largest, second_l;
-	
-	public int largestRectangleInHistogram(int[] heights) {
+	public boolean checkPairwiseConsecutive(LinkedStack<Integer> iStk) {
+		LinkedStack<Integer> aux = new LinkedStack<>();
 		
-		int n = heights.length;
-		
-		int maxArea = 0;
-		
-		int[] ns = new int[n];
-		int[] ps = new int[n];
-		
-		ns = nextSmallerElement(heights);
-		ps = prevSmallerElement(heights);
-		
-		for(int i = 0; i < n; i++) {
-			
-			// if there is no next smaller, then -1 would cause problem in area calculation
-			// hence we increase next smaller to n
-			if(ns[i] == -1) {
-				ns[i] = n;
-			}
-			
-			int currArea = (ns[i] - ps[i] - 1) * heights[i];
-			
-			maxArea = Math.max(maxArea, currArea);
-			
-			System.out.println("Area considering " + heights[i] + " is " + currArea);
-			
+		while(!iStk.isEmpty()) {
+			aux.push(iStk.pop());
 		}
 		
-		return maxArea;
-	}
-	
-	int[] nextSmallerElement(int[] a) {
-		int[] ns = new int[a.length];
+		boolean result = true;
 		
-		LinkedStack<Integer> stk = new LinkedStack<>();
-		
-		// we start from n-1 because, for last element,
-		// next smaller will be -1, as nothing is there on right of last
-		// so, we begin next smaller calculation from last element for stack approach
-		for(int i = a.length - 1; i >= 0; i--) {
+		while(aux.size() > 1) {
+			int v1 = aux.pop();
+			int v2 = aux.pop();
 			
-			// until stack has some contents, and element referred by top of stack is greater than current element of array
-			// keep popping from stack, until we find a smaller element index in stack
-			while(!stk.isEmpty() && a[stk.peek()] >= a[i]) {
-				stk.pop();
+			if(Math.abs(v2-v1) != 1) {
+				return false;
 			}
 			
+			iStk.push(v1);
+			iStk.push(v2);
 			
-			// if stack is empty, there is no next smaller for current element
-			// otherwise, if stack is non-empty, top of stack index is next smaller
-			if(stk.isEmpty()) {
-				ns[i] = -1;
-			} else {
-				ns[i] = stk.peek();
+			if(aux.size() == 1) {
+				iStk.push(aux.pop());
 			}
-			
-			// every time, we push the index of element, not the element
-			stk.push(i);
 		}
-		
-		return ns;
+		return result;
 	}
-	
-	int[] prevSmallerElement(int[] a) {
-		int[] ps = new int[a.length];
-		
-		LinkedStack<Integer> stk = new LinkedStack<>();
-		
-		// for first element, prev smaller will be -1 itself
-		// hence we begin prev smaller calculation from start of array
-		for(int i = 0; i < a.length; i++) {
-			// until stack has some contents, and element referred by top of stack is greater than current element of array
-			// keep popping from stack, until we find a smaller element index in stack
-			while(!stk.isEmpty() && a[stk.peek()] >= a[i]) {
-				stk.pop();
-			}
-			
-			// if stack is empty, there is no prev smaller for current element
-			// otherwise, if stack is non-empty, top of stack index is prev smaller
-			if(stk.isEmpty()) {
-				ps[i] = -1;
-			} else {
-				ps[i] = stk.peek();
-			}
-			
-			// every time, we push the index of element, not the element
-			stk.push(i);
-		}
-		
-		return ps;
-		
-	}
-	
 	
 	
 	public static void main(String[] args) {
 		_1_a_TestClass<String> mainClass = new _1_a_TestClass<>();
 		
-		int[] inp = new int[] {2, 3, 1, 6, 9, 14, 8, 7};
+		LinkedStack<Integer> iStk = new LinkedStack<>();
 		
-		System.out.println("Input: " + Arrays.toString(inp));
+		iStk.push(7);
+		iStk.push(6);
+		iStk.push(2);
+		iStk.push(3);
+		iStk.push(34);
+		iStk.push(33);
+		iStk.push(98);
+		System.out.println("Input stack: " + iStk.toString());
 		
-		int maxArea = mainClass.largestRectangleInHistogram(inp);
+		boolean isPairwiseConsecutive = mainClass.checkPairwiseConsecutive(iStk);
 		
-		System.out.println("Max Area: " + maxArea);
+		System.out.println("Is stack pairwise consecutive?: " + isPairwiseConsecutive);
 	}
 }
