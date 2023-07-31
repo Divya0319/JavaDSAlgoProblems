cfb55ae216ee26deeed83a45128b22ab2cdd72e9
Divya Gupta <divygupta0319@gmail.com>
2023-07-31 21:31:52 +0530

    Replaced memorization code involving recursion, to a tabulation code having no recursion, for 0-1 knapsack problem

--- src/main/java/NarasimhaKarumanchi/java/_7_GreedyApproaches/_1_b_0_1_KnapSack.java
+++ src/main/java/NarasimhaKarumanchi/java/_7_GreedyApproaches/_1_b_0_1_KnapSack.java
@@ -10,34 +10,44 @@ package main.java.NarasimhaKarumanchi.java._7_GreedyApproaches;
  * As there might be no uniformity, that summing up profit of two other items might have more profit, 
  * than summing up single item having max profit.
  * 
- * So, we will try out all combinations, and among those combinations, we will take the best total value giving combination, 
- * using Recursion.
+ * Here we are converting Recursion + Dynamic Programming i.e. Memorization approach to Tabulation
+ * which doesn't require recursion stack. And every calculation is performed and used from inside dp array itself.
  * 
  */
 public class _1_b_0_1_KnapSack {	
 	
 	
-	public int _01_KnapsackSolution(int ind, int W, int[] wt, int[] val, int[][] dp) {
+	public int _01_KnapsackSolution(int[] wt, int[] val, int n, int maxWeight) {
 		
-		if(ind == 0) {
-			if(wt[0] <= W) {
-				return val[0];
-			} else {
-				return 0;
+		int[][] dp = new int[n][maxWeight+1];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j <= maxWeight; j++) {
+				dp[i][j] = 0;
 			}
 		}
 		
-		int notTake = 0 + _01_KnapsackSolution(ind - 1, W, wt, val, dp);
-		
-		int take = Integer.MIN_VALUE;
-		
-		if(wt[ind] <= W) {
-			take = val[ind] + _01_KnapsackSolution(ind - 1, W - wt[ind], wt, val, dp);
+		for(int W = wt[0]; W <= maxWeight; W++) {
+			dp[0][W] = val[0];
 		}
 		
+		for(int ind = 1; ind < n; ind++) {
+			for(int W = 0; W <= maxWeight; W++) {
+				int notTake = 0 + dp[ind - 1][W];
+				
+				int take = Integer.MIN_VALUE;
+				
+				if(wt[ind] <= W) {
+					take = val[ind] + dp[ind - 1][W - wt[ind]];
+				}
+				
+				
+				
+				dp[ind][W] = Math.max(notTake, take);
+			}
+		}
 		
-		
-		return dp[ind][W] = Math.max(notTake, take);
+		 return dp[n-1][maxWeight];
 		
 	}
 	
@@ -53,15 +63,7 @@ public class _1_b_0_1_KnapSack {
 		
 		int n = wt.length;
 		
-		int[][] dp = new int[n][maxWeight+1];
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j <= maxWeight; j++) {
-				dp[i][j] = -1;
-			}
-		}
-		
-		int maxProfit = mainClass._01_KnapsackSolution(n - 1, maxWeight, wt, val, dp);
+		int maxProfit = mainClass._01_KnapsackSolution(wt, val, n, maxWeight);
 		
 		System.out.println("Max profit: " + maxProfit);
 		
