79e6a6b21a40d9fe33656d2d73803acf60a07059
Divya Gupta <divygupta0319@gmail.com>
2023-09-06 11:45:34 +0530

    Added problem and solution for top, bottom, left and right views of binary tree, alongwith, diagonal traversal and sum of nodes in longest bloodline

---
+++ src/main/java/NarasimhaKarumanchi/java/_4_Trees/_3_FANNG_Problems/_4_l_BottomViewOfBinaryTree.java
@@ -0,0 +1,94 @@
+package main.java.NarasimhaKarumanchi.java._4_Trees._3_FANNG_Problems;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import main.java.NarasimhaKarumanchi.java._4_Trees.BinaryTreeNode;
+import main.java.NarasimhaKarumanchi.java._4_Trees.LinkedQueue;
+
+public class _4_l_BottomViewOfBinaryTree {
+
+	ArrayList<Integer> bottomView(BinaryTreeNode<Integer> root) {
+		
+		ArrayList<Integer> ans = new ArrayList<>();
+		
+		// base case
+		if(root == null) {
+			return ans;
+		}
+		
+		Map<Integer, Integer> topNode = new TreeMap<>();
+		LinkedQueue<Pair> q = new LinkedQueue<>();
+		
+		try {
+			
+			q.enQueue(new Pair(0, root));
+			
+			while(!q.isEmpty()) {
+				Pair temp = q.deQueue();
+				BinaryTreeNode<Integer> frontNode = temp.node;
+				int hd = temp.hd;
+				
+				// if one value is present for a horizontal distance, we simply replace it
+				topNode.put(hd, frontNode.getData());
+				
+				
+				if(frontNode.getLeft() != null) {
+					q.enQueue(new Pair(hd-1, frontNode.getLeft()));
+				}
+				
+				if(frontNode.getRight() != null) {
+					q.enQueue(new Pair(hd+1, frontNode.getRight()));
+				}
+			}
+			
+			for(Map.Entry<Integer, Integer> entry : topNode.entrySet()) {
+				ans.add(entry.getValue());
+			}
+			
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return ans;
+
+	}
+
+	public static void main(String[] args) {
+		_4_l_BottomViewOfBinaryTree mainClass = new _4_l_BottomViewOfBinaryTree();
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
+		BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
+		BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(3);
+		BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>(4);
+		BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>(5);
+		BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(6);
+		BinaryTreeNode<Integer> node7 = new BinaryTreeNode<>(7);
+		BinaryTreeNode<Integer> node8 = new BinaryTreeNode<>(8);
+		BinaryTreeNode<Integer> node9 = new BinaryTreeNode<>(9);
+
+		root.setLeft(node2);
+		root.setRight(node3);
+		node2.setLeft(node4);
+		node2.setRight(node5);
+		node3.setLeft(node6);
+		node3.setRight(node7);
+		node6.setRight(node8);
+		node7.setRight(node9);
+
+		System.out.println(mainClass.bottomView(root));
+	}
+	
+	class Pair {
+		int hd;
+		BinaryTreeNode<Integer> node;
+		
+		public Pair(int hd, BinaryTreeNode<Integer> node) {
+			this.hd = hd;
+			this.node = node;
+		}
+	}
+
+}
