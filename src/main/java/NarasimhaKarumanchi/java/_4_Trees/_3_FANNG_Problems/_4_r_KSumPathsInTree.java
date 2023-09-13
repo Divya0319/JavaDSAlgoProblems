8a4e355bb40005ec38fc58aa8a5eb4222145c0bc
Divya Gupta <divygupta0319@gmail.com>
2023-09-13 23:45:11 +0530

    Updated driver code for k sum path and maximum sum of non adjacent nodes problem of tree

--- src/main/java/NarasimhaKarumanchi/java/_4_Trees/_3_FANNG_Problems/_4_r_KSumPathsInTree.java
+++ src/main/java/NarasimhaKarumanchi/java/_4_Trees/_3_FANNG_Problems/_4_r_KSumPathsInTree.java
@@ -47,26 +47,50 @@ public class _4_r_KSumPathsInTree {
 	public static void main(String[] args) {
 		_4_r_KSumPathsInTree mainClass = new _4_r_KSumPathsInTree();
 
+//		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
+//		BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
+//		BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(3);
+//		BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>(4);
+//		BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>(5);
+//		BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(6);
+//		BinaryTreeNode<Integer> node7 = new BinaryTreeNode<>(7);
+//		BinaryTreeNode<Integer> node8 = new BinaryTreeNode<>(8);
+//		BinaryTreeNode<Integer> node9 = new BinaryTreeNode<>(9);
+//
+//		root.setLeft(node2);
+//		root.setRight(node3);
+//		node2.setLeft(node4);
+//		node2.setRight(node5);
+//		node3.setLeft(node6);
+//		node3.setRight(node7);
+//		node6.setRight(node8);
+//		node7.setRight(node9);
+		
 		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
-		BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
 		BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(3);
+		BinaryTreeNode<Integer> nodeminus1 = new BinaryTreeNode<>(-1);
+		BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
 		BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>(4);
 		BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>(5);
+		BinaryTreeNode<Integer> node1_1 = new BinaryTreeNode<>(1);
+		BinaryTreeNode<Integer> node1_1_1 = new BinaryTreeNode<>(1);
+		BinaryTreeNode<Integer> node1_1_1_1 = new BinaryTreeNode<>(1);
+		BinaryTreeNode<Integer> node2_2 = new BinaryTreeNode<>(2);
 		BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(6);
-		BinaryTreeNode<Integer> node7 = new BinaryTreeNode<>(7);
-		BinaryTreeNode<Integer> node8 = new BinaryTreeNode<>(8);
-		BinaryTreeNode<Integer> node9 = new BinaryTreeNode<>(9);
 
-		root.setLeft(node2);
-		root.setRight(node3);
-		node2.setLeft(node4);
-		node2.setRight(node5);
-		node3.setLeft(node6);
-		node3.setRight(node7);
-		node6.setRight(node8);
-		node7.setRight(node9);
+		root.setLeft(node3);
+		root.setRight(nodeminus1);
+		node3.setLeft(node2);	
+		node3.setRight(node1_1);
+		nodeminus1.setLeft(node4);
+		nodeminus1.setRight(node5);
+		node1_1.setLeft(node1_1_1);
+		node4.setLeft(node1_1_1_1);
+		node4.setRight(node2_2);
+		node5.setRight(node6);
+		
 
-		System.out.println(mainClass.sumK(root, 7));
+		System.out.println(mainClass.sumK(root, 5));
 	}
 
 }
--- src/main/java/NarasimhaKarumanchi/java/_4_Trees/_3_FANNG_Problems/_4_t_MaximumSumOfNonAdjacentNodes.java
+++ src/main/java/NarasimhaKarumanchi/java/_4_Trees/_3_FANNG_Problems/_4_t_MaximumSumOfNonAdjacentNodes.java
@@ -31,24 +31,59 @@ public class _4_t_MaximumSumOfNonAdjacentNodes {
 	public static void main(String[] args) {
 		_4_t_MaximumSumOfNonAdjacentNodes mainClass = new _4_t_MaximumSumOfNonAdjacentNodes();
 
+//		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
+//		BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
+//		BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(3);
+//		BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>(4);
+//		BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>(5);
+//		BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(6);
+//		BinaryTreeNode<Integer> node7 = new BinaryTreeNode<>(7);
+//		BinaryTreeNode<Integer> node8 = new BinaryTreeNode<>(8);
+//		BinaryTreeNode<Integer> node9 = new BinaryTreeNode<>(9);
+//
+//		root.setLeft(node2);
+//		root.setRight(node3);
+//		node2.setLeft(node4);
+//		node2.setRight(node5);
+//		node3.setLeft(node6);
+//		node3.setRight(node7);
+//		node6.setRight(node8);
+//		node7.setRight(node9);
+		
 		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
 		BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
 		BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(3);
+		BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(6);
 		BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>(4);
 		BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>(5);
-		BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(6);
-		BinaryTreeNode<Integer> node7 = new BinaryTreeNode<>(7);
+		BinaryTreeNode<Integer> node16 = new BinaryTreeNode<>(16);
 		BinaryTreeNode<Integer> node8 = new BinaryTreeNode<>(8);
 		BinaryTreeNode<Integer> node9 = new BinaryTreeNode<>(9);
+		BinaryTreeNode<Integer> node21 = new BinaryTreeNode<>(21);
+		BinaryTreeNode<Integer> node11 = new BinaryTreeNode<>(11);
+		BinaryTreeNode<Integer> node15 = new BinaryTreeNode<>(15);
+		BinaryTreeNode<Integer> node70 = new BinaryTreeNode<>(70);
+		BinaryTreeNode<Integer> node18 = new BinaryTreeNode<>(18);
+		BinaryTreeNode<Integer> node65 = new BinaryTreeNode<>(65);
+		BinaryTreeNode<Integer> nodeminus8 = new BinaryTreeNode<>(-8);
 
 		root.setLeft(node2);
 		root.setRight(node3);
-		node2.setLeft(node4);
-		node2.setRight(node5);
-		node3.setLeft(node6);
-		node3.setRight(node7);
+		node2.setLeft(node6);
+		node3.setLeft(node4);
+		node3.setRight(node5);
+		node6.setLeft(node16);
 		node6.setRight(node8);
-		node7.setRight(node9);
+		node16.setLeft(node11);
+		node16.setRight(node15);
+		node8.setRight(node70);
+		node4.setLeft(node9);
+		node5.setLeft(node21);
+		node9.setRight(node18);
+		node21.setLeft(node65);
+		node21.setRight(nodeminus8);
+
+		
 
 		System.out.println(mainClass.getMaxSum(root));
 	}
