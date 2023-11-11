e2f1c1bc950e47aad3de7348b445aa33880b1607
Divya Gupta <divygupta0319@gmail.com>
2023-11-11 13:26:59 +0530

    Implemented a naive approach to find out largest prime in  a given range of natural numbers, by checking till sqrt of highest range given

---
+++ src/main/java/NarasimhaKarumanchi/java/LargestPrime.java
@@ -0,0 +1,37 @@
+package main.java.NarasimhaKarumanchi.java;
+
+public class LargestPrime {
+	
+	public void largestPrime() {
+		
+		
+		for(int i = 100000000; i >= 2; i--) {
+			if(isPrime(i)) {
+				System.out.println(i);
+				break;
+			}
+			
+		}
+		
+		
+	}
+	
+	
+	// gives if number is prime
+	public boolean isPrime(int n) {
+		for(int i = 2; i < n/2; i++) {
+			if(n % i == 0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		
+		LargestPrime lp = new LargestPrime();
+		lp.largestPrime();
+	}
+
+}
